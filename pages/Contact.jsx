edac7d5d8b64dd4c5dda6829b1bf01cb1d439fc3
percivalxxvi <ayodejioladeji12@gmail.com>
2025-12-19 21:40:44 +0100

    v2.3

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -156,6 +156,21 @@ const Contact = () => {
           </motion.button>
         </form>
       </motion.section>
+      {/* Footer */}
+      <footer className="flex items-center justify-between bg-[#f2c311] text-black py-4 px-6 text-center font-semibold text-sm">
+        © {new Date().getFullYear()} Olamifeng · Nigeria · All Rights Reserved
+        <h1 className="font-light">
+          Powered by{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://greylinex.vercel.app/"
+            className="font-bold"
+          >
+            AOD Interactive
+          </a>{" "}
+        </h1>
+      </footer>
     </main>
   );
 };
