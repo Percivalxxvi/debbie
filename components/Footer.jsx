d68c4d733f9865ec9d0fbfa56594809839427ab6
percivalxxvi <ayodejioladeji12@gmail.com>
2025-12-19 22:06:24 +0100

    v2.5.1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flex lg:flex-row flex-col items-center justify-between bg-[#f2c311] text-black py-4 lg:px-6 text-center font-semibold text-sm">
+    <footer className="flex lg:flex-row flex-col items-center justify-between bg-[#f2c311] text-black lg:py-4 py-2 lg:px-6 text-center font-semibold text-sm lg:gap-0 gap-2">
       © {new Date().getFullYear()} Olamifeng · Nigeria · All Rights Reserved
       <h1 className="font-light">
         Powered by{" "}
